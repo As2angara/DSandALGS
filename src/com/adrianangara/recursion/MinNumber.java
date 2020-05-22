3ee6fcabe536ac16fc0269d8aa716bc84d1c359b
AdrianSA <Adrian.angara@gmail.com>
2020-05-22 12:40:06 -0400

    Added MinNumber class

---
+++ src/com/adrianangara/recursion/MinNumber.java
@@ -0,0 +1,14 @@
+package com.adrianangara.recursion;
+
+public class MinNumber {
+
+    //Given an array, find the min value in the array using recursion
+
+    //Implementation #1
+    public int getMin(int[] arr, int i, int n) {
+
+        return 0;
+
+    }
+
+}
