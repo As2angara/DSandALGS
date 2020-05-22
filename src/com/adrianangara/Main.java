b1136156b16f1b398ec7339f7e8660afce33c863
AdrianSA <Adrian.angara@gmail.com>
2020-05-22 11:21:03 -0400

    Implemented the ReverseAString function

--- src/com/adrianangara/Main.java
+++ src/com/adrianangara/Main.java
@@ -1,17 +1,27 @@
 package com.adrianangara;
 
+import com.adrianangara.recursion.ReverseAString;
 import com.adrianangara.recursion.SumOfSquares;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         //Calculate the sum of squares between 7 and 10
-        int n =7, m =10;
-        SumOfSquares sos = new SumOfSquares();
-//        int sum = sos.sumOfSquares2(n, m); //Implementation #1
-        int sum = sos.sumOfSquares2(n, m); //Implementation #2
-        System.out.println("The sum of squares between " + n + " and " + m + " is: " + sum);
+//        int n =7, m =10;
+//        SumOfSquares sos = new SumOfSquares();
+////        int sum = sos.sumOfSquares2(n, m); //Implementation #1
+//        int sum = sos.sumOfSquares2(n, m); //Implementation #2
+//        System.out.println("The sum of squares between " + n + " and " + m + " is: " + sum);
+
+        //Reverse a string inputted by the user
+        Scanner in = new Scanner(System.in);
+        ReverseAString rev = new ReverseAString();
+
+        String s = in.nextLine();
+        System.out.println(rev.reverseAString(s));
 
 
 
--- src/com/adrianangara/recursion/ReverseAString.java
+++ src/com/adrianangara/recursion/ReverseAString.java
@@ -1,4 +1,34 @@
 package com.adrianangara.recursion;
 
 public class ReverseAString {
+
+    //Reverse a string that has been inputted by the user
+
+    //Implementation #1
+    public String reverseAString(String s) {
+
+        //If empty string, return
+        if (s.equals("")) {
+            return "";
+        }
+
+        //Get the first character
+        String c0 = s.substring(0, 1);
+
+        //Get the last character
+        String cn = s.substring(s.length() - 1, s.length());
+
+        //Get the index of each character
+        int n = s.indexOf(c0);
+        int m = s.lastIndexOf(cn);
+
+        //return nothing in the indexes are the same
+        if (n == m) {
+            return s;
+        }
+
+
+        return cn + reverseAString(s.substring(1, s.length() - 1)) + c0;
+    }
+
 }
