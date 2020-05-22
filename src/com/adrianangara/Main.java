90dfbb4f1ebb8e1f0bc3ad099f3f84c94a125953
AdrianSA <Adrian.angara@gmail.com>
2020-05-22 10:47:43 -0400

    Implemented a second version of sum of squares recursive function

--- src/com/adrianangara/Main.java
+++ src/com/adrianangara/Main.java
@@ -8,11 +8,12 @@ public class Main {
 
         //Calculate the sum of squares between 7 and 10
         int n =7, m =10;
-
         SumOfSquares sos = new SumOfSquares();
-
-        int sum = sos.sumOfSquares(n, m);
-
+//        int sum = sos.sumOfSquares2(n, m); //Implementation #1
+        int sum = sos.sumOfSquares2(n, m); //Implementation #2
         System.out.println("The sum of squares between " + n + " and " + m + " is: " + sum);
+
+
+
     }
 }
--- src/com/adrianangara/recursion/SumOfSquares.java
+++ src/com/adrianangara/recursion/SumOfSquares.java
@@ -5,12 +5,24 @@ public class SumOfSquares {
     //Find the sum of squares S(n,m) of integers that fall within a range [n,m]
     //Ex: [7, 10] -> return sum = 7*7 + 8*8 + 9*9 + 10*10
 
+    //Implementation #1
     public int sumOfSquares(int n, int m) {
 
         if (n == m) {
             return n*n;
         } else {
             return m*m + sumOfSquares(n, m - 1);
+        }
+
+    }
+
+    //Implementation #2
+    public int sumOfSquares2(int n, int m) {
+
+        if (n == m) {
+            return n*n;
+        } else {
+            return sumOfSquares2(n, (m+n)/2) + sumOfSquares2(((m+n)/2) + 1, m);
         }
 
     }
